de9db9fe4bdf0f196b86a71f6ba5eab9f6abd138
marsante <github@brunoy-osteopathe.fr>
2023-07-21 22:18:40 +0200

    fix: headers templates

--- upgrade/osteo/sqlInstall.sql
+++ upgrade/osteo/sqlInstall.sql
@@ -281,4 +281,4 @@ INSERT IGNORE INTO `forms` (`module`, `internalName`, `name`, `description`, `da
 
 -- system
 INSERT IGNORE INTO `system` (`name`, `groupe`, `value`) VALUES
-('osteo', 'module', 'v0.5.1');
+('osteo', 'module', 'v0.5.2');
---
+++ upgrade/osteo/sqlUpgrade_v0.5.1_v0.5.2.sql
@@ -0,0 +1,3 @@
+-- Modifications des données de la bdd
+
+UPDATE `system` SET `value`='v0.5.2' WHERE `groupe`='module' and `name`='osteo';
